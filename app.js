f236c34fee3a25910a72dda5785ac99d8a4c4373
Catherine Crenshaw <55335367+ccrenshawcodes@users.noreply.github.com>
2023-09-06 15:18:41 -0600

    Calculate total severity and update DOM to reflect result

--- app.js
+++ app.js
@@ -1,32 +1,80 @@
 
 function toggleSelectedStatus(box) {
-    /* assumes:
-        HTML includes id elements which define the column each cell is in
-        HTML boxes/cells have a "box" class 
-
-        Colors are just to show what's going on.
-
-        "selected" status is first removed from any element in the column where it was applied
-        Then "selected" status is added to the clicked element*/
-
     const columnCells = document.querySelectorAll(`#${box.id}`);
     columnCells.forEach(cell => {
-        cell.style.color = 'purple';
         cell.classList.remove('selected');
+        cell.style.cssText = 'background-color: inherit;';
     })
 
-    box.style.color = 'orange';
     box.classList.toggle('selected');
+    setSelectedColors(box);
+}
+
+function setSelectedColors (box) {
+    const weight = box.getAttribute('data-weight');
+    if (weight >= 8) {
+        box.style.cssText = 'background-color: #f5006a;';
+    } else if (weight <= 7 && weight > 4) {
+        box.style.cssText = 'background-color: #FFBE0B;';
+    } else if (weight <= 4 && weight > 0) {
+        box.style.cssText = 'background-color: #9757EF;';
+    } else if (weight < 1) {
+        box.style.cssText = 'background-color: #27CE2D;';
+    }
+}
+
+function calculateTotals () {
+    const selectedBoxes = document.querySelectorAll('.selected');
+    const weightsArray = [];
+
+    const recordBoxWeight = (item, arr) => {
+        const weight = item.getAttribute('data-weight');
+        arr.push(Number(weight));
+        return arr;
+    }
+    
+    selectedBoxes.forEach(box => {
+        recordBoxWeight(box, weightsArray);
+    })
+
+    console.log(weightsArray);
+
+    if (weightsArray.length >= 5) {
+        updateResult(weightsArray);
+    }
+}
+
+function updateResult (arr) {
+    let totals = arr.reduce((a, b) => a + b);
+    console.log(`severity score: ${totals}`);
+
+    const result = document.querySelector('.result-text');
+    const resultBox = document.querySelector('.result');
+
+    if (totals >= 30) {
+        result.textContent = 'Severity 1';
+        resultBox.style.cssText = 'background-color: #f5006a;';
+    } else if (totals < 30 && totals >= 20) {
+        result.textContent = 'Severity 2';
+        resultBox.style.cssText = 'background-color: #FFBE0B;';
+    } else if (totals < 20 && totals >= 12) {
+        result.textContent = 'Severity 3';
+        resultBox.style.cssText = 'background-color: #9757EF;';
+    } else if (totals < 12) {
+        result.textContent = 'Severity 4';
+        resultBox.style.cssText = 'background-color: #27CE2D;';
+    }
 }
 
 function makeBoxesClickable () {
-    //  Executes toggleSelectedStatus function when a box is clicked
     const boxes = document.querySelectorAll('.box');
     boxes.forEach(box => {
         box.addEventListener('click', () => {
             toggleSelectedStatus(box);
+            calculateTotals();
         });
     })
 }
 makeBoxesClickable();
+
 
